7470daba6ad98e6e081439bb1741f6f0bad83d53
Aristocbrat <emmanuelforbez@gmail.com>
2024-09-02 12:57:22 +0100

    gg

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -39,7 +39,7 @@ const Discount = () => {
       <Nav2/>
        <div className="regis sticky top-[0%] z-20 w-full bg-white  dark:bg-black dark:text-white flex justify-between xl:px-7 sm:px-2 py-4">
   <h1 className='xl:text-3xl sm:text-[12px] font-bold sm:mt-2'>Sell with 3rdGB</h1>
-  <h1 className='flex py-2 font-semibold sm:text-[10px] xl:text-lg xl:w-[250px] sm:w-[150px]'>
+  <h1 className='flex py-2 font-semibold sm:text-[10px] xl:text-lg xl:w-[250px] sm:w-[120px]'>
     <RiDiscountPercentFill className='xl:text-3xl sm:text-[20px] text-green-500' /> 
     Get up to 10% on your first #50,000 in branded sales.
     
