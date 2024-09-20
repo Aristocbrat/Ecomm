5ecfacd393b6612df835acdaa229284f47a898ce
Aristocbrat <emmanuelforbez@gmail.com>
2024-09-20 15:59:07 +0100

    last commit

--- src/components/extras/Summary.jsx
+++ src/components/extras/Summary.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import productdata11 from '../datasfolder/Data11';
 import Summarycard from '../itemcardss/Summarycard';
 import Slider from 'react-slick';
@@ -6,7 +6,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Summary = () => {
-    const settings = {
+    const [sliderSettings, setSliderSettings]  =useState( {
+      swipeToSlide: false,
         dots: true,
         infinite: true,
         speed: 500,
@@ -39,13 +40,41 @@ const Summary = () => {
             }
           ]
      
-    };
+    });
+    useEffect(() => {
+      const mobileResize = () => {
+        if (window.innerWidth <= 768) {
+          setSliderSettings((prevSettings) => ({
+            ...prevSettings,
+            swipeToSlide: true,
+            slidesToShow: 2, // Adjust number of slides for mobile
+          }));
+        } else {
+          setSliderSettings((prevSettings) => ({
+            ...prevSettings,
+            swipeToSlide: false,
+            slidesToShow: 3, // Number of slides for larger screens
+          }));
+        }
+      };
+  
+      // Call the function once when component mounts
+      mobileResize();
+  
+      // Add resize event listener
+      window.addEventListener('resize', mobileResize);
+  
+      // Cleanup event listener on component unmount
+      return () => {
+        window.removeEventListener('resize', mobileResize);
+      };
+    }, []);
 
     return (
         <div className='hide-scrollbar py-4 dark:text-white dark:bg-[#1f4550]' id='Review'>
             <section className='py-6 xl:m-auto xl:w-full'>
                 <h1 className='xl:text-4xl font-extrabold flex justify-center py-3'>Customer's Review</h1>
-                <Slider {...settings}>
+                <Slider {...sliderSettings}>
                     {productdata11.map((item, index) => (
                         <div key={index} className="customer py-3 flex xl:ml-8 px-10  ">
                             <Summarycard
